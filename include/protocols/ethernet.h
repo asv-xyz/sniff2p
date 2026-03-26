39818bde65df9c97ee64cd5b4eb539ab5bd34d4e
asv-xyz <alex.sukhorukov123@yandex.ru>
2026-03-26 20:48:24 +0300

    Added ethernet header and types

---
+++ include/protocols/ethernet.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <types.h>
+
+#pragma pack(push, 1)
+typedef struct _EthernetHeader {
+    U8  dst_mac[6];
+    U8  src_mac[6];
+    U16 ether_type;
+} EthernetHeader;
+
+#pragma pack(pop)
---
+++ include/utils/types.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cstdint>
+
+using U8    = uint8_t;
+using U16   = uint16_t;
+using U32   = uint32_t;
+using U64   = uint64_t;
