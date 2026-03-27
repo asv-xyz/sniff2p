3f32210167006974a8b1ce82e54f578ee33d7c38
asv-xyz <alex.sukhorukov123@yandex.ru>
2026-03-27 09:52:25 +0300

    Added ICMP

---
+++ include/protocols/icmp.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "types.h"
+
+#pragma pack(push, 1)       // disabling allingment
+typedef struct _ICMPHeader {
+    U8  type;
+    U8  code;
+    U16 checksum;
+
+    U32 ext_h;              // extended header
+    U8  data[0];
+} ICMPHeader;
+
+#pragma pack(pop)           // enabling allingment
