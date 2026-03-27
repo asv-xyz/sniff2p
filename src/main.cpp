c5bd84db249062b2c9604bcc378060863cf7c881
asv-xyz <alex.sukhorukov123@yandex.ru>
2026-03-27 11:22:03 +0300

    Added socket creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/if_ether.h>
+#include <unistd.h>
+
+#include "protocols/ethernet.h"
+#include "protocols/icmp.h"
+#include "protocols/ipv4.h"
+#include "protocols/tcp.h"
+#include "utils/types.h"
+
 int main(void) {
-	return 0;
+    int sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    if (sockfd < 0) {
+        std::cerr << "Couldn't create socket (try with sudo)" << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::cout << "Socket successfully created!" << std::endl;
+
+    close(sockfd);
+	return EXIT_SUCCESS;
 }
